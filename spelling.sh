e02ccce336caa9c75e996865790b6233d69d936b
Laszlo <pityq1@gmail.com>
2021-09-29 18:49:40 +0100

    add file

---
+++ spelling.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+say good luck Anna
+
+for value in vicious gracious spacious malicious precious conscious delicious suspicious atrocious ferocious   bicycle believe
+do
+    say $value
+    sleep 8
+done
