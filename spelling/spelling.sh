772bd59a4c4e733b6df8d1c379c83bbc4a724afb
Laszlo <pityq1@gmail.com>
2021-10-06 18:22:53 +0100

    child name as parameter

--- spelling/spelling.sh
+++ spelling/spelling.sh
@@ -1,9 +1,15 @@
 #!/bin/sh
 
-say good luck Anna
+if [ $# -ne 2 ]; then
+    echo "Usage: spelling.sh child_name file_path" >&2
+    exit 2
+fi
+
+
+say "good luck $1"
 
 
 while IFS= read -r line; do
     say $line
     sleep 8
-done < "$1"
+done < "$2"
